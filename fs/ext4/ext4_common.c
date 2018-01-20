c5d4a8d00706c50c1088f8caeb71499655eff035
Robert Nelson <robertcnelson@gmail.com>
2018-01-20 12:43:22 -0600

    am335x_evm: uEnv.txt, bootz, n fixes
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- fs/ext4/ext4_common.c
+++ fs/ext4/ext4_common.c
@@ -2378,7 +2378,7 @@ int ext4fs_mount(unsigned part_length)
 
 	return 1;
 fail:
-	printf("Failed to mount ext2 filesystem...\n");
+	//printf("Failed to mount ext2 filesystem...\n");
 	free(data);
 	ext4fs_root = NULL;
 
